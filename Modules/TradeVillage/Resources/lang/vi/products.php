6e3d636f68e671b2ac50caaf24e1a859f3c2f199
tuyettran <tuyet.tran.k59@gmail.com>
2018-02-02 00:28:29 +0700

    products crud admin

---
+++ Modules/TradeVillage/Resources/lang/vi/products.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'list resource' => 'Danh sách sản phẩm',
+    'create resource' => 'Thêm sản phẩm',
+    'edit resource' => 'Sửa sản phẩm',
+    'destroy resource' => 'Xóa sản phẩm',
+    'title' => [
+        'products' => 'Sản phẩm',
+        'create products' => 'Thêm sản phẩm',
+        'edit products' => 'Sửa sản phẩm',
+    ],
+    'button' => [
+        'create products' => 'Thêm sản phẩm',
+    ],
+    'table' => [
+        'name' => 'Tên',
+        'description' => 'Mô tả',
+        'owner' => 'Chủ sản phẩm',
+        'image' => 'Ảnh',
+        'cost' => 'Giá',
+        'material' => 'Chất liệu',
+        'actions' => 'Hành động',
+    ],
+    'form' => [
+        'name' => 'Tên',
+        'description' => 'Mô tả',
+        'owner' => 'Chủ sản phẩm',
+        'image' => 'Ảnh',
+        'cost' => 'Giá',
+        'material' => 'Chất liệu',
+        'actions' => 'Hành động',
+        'enterprise' => 'Doanh nghiệp',
+        'artist' => 'Nghệ nhân',
+        'detail' => 'Chi tiết',
+        'visitor_counter' => 'Số lượt xem',
+        'model' => 'Mô hình 3D',
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
