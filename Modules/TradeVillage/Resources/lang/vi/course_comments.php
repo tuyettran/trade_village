34c11a7ad727e09a78a96870c464acc43340e072
ngocnhu123 <nhuwhn96@gmail.com>
2018-02-01 16:00:26 +0700

    add create and delete course comments

---
+++ Modules/TradeVillage/Resources/lang/vi/course_comments.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'list resource' => 'Danh sách bình luận các khóa học',
+    'destroy resource' => 'Xóa bình luận',
+    'title' => [
+        'course_comments' => 'Bình luận các khóa học',
+    ],
+    
+    'table' => [
+        'no' => 'No',
+        'content' => 'Nội dung',
+        'user' => 'Người bình luận',
+        'course' => 'Khóa học',
+        'actions'=> 'Hành động',
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
