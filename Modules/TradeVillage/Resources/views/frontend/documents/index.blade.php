ecf7243817d0249f670d291ce5c3e3c10cfdfd66
tuyettran <tuyet.tran.k59@gmail.com>
2018-02-21 20:49:04 +0700

    add frontend base layout

---
+++ Modules/TradeVillage/Resources/views/frontend/documents/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('style')
+@stop
+
+@section('content')
+	awewewe
+	awewewe
+	<div>
+		cafafe
+	</div>
+	<ul>
+		<li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li><li>àhej
+		aefw</li>
+	</ul>
+@stop
+
+@section('scripts')
+@stop
--- Themes/Flatly/views/layouts/master.blade.php
+++ Themes/Flatly/views/layouts/master.blade.php
@@ -10,14 +10,19 @@
         @section('title')@setting('core::site-name')@show
     </title>
     <link rel="shortcut icon" href="{{ Theme::url('favicon.ico') }}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 
     {!! Theme::style('css/main.css') !!}
+    {!! Theme::style('css/style.css') !!}
+    {!! Theme::style('css/header_footer.css') !!}
+
+    @yield('style')
 </head>
 <body>
-
+@include('partials.header')
 @include('partials.navigation')
 
-<div class="container">
+<div class="custom-container">
     @yield('content')
 </div>
 @include('partials.footer')
@@ -28,5 +33,18 @@
 <?php if (Setting::has('core::analytics-script')): ?>
     {!! Setting::get('core::analytics-script') !!}
 <?php endif; ?>
+
+    <script type="text/javascript">
+        $(window).scroll(function(e){ 
+        var $el = $('.super-header'); 
+
+        if ($(this).scrollTop() > 200){ 
+                $('.navbar').addClass("navbar-fixed-top");
+            }
+        if ($(this).scrollTop() < 200){
+                $('.navbar').removeClass("navbar-fixed-top");
+        } 
+    }); 
+    </script>
 </body>
 </html>
