75164049aaa699058a5de67628db1a81feca63d8
tuyettran <tuyet.tran.k59@gmail.com>
2018-03-20 08:33:42 +0700

    add rate to product

--- Modules/TradeVillage/Http/frontendRoutes.php
+++ Modules/TradeVillage/Http/frontendRoutes.php
@@ -82,12 +82,20 @@ $router->group(['prefix' =>'/tradevillage'], function (Router $router) {
         'as' => 'frontend.tradevillage.artist.index',
         'uses' => 'FrontendArtistController@index'
     ]);
+    $router->get('artists/{artist}', [
+        'as' => 'frontend.tradevillage.artist.show',
+        'uses' => 'FrontendArtistController@show'
+    ]);
     $router->bind('events', function ($id) {
         return app('Modules\TradeVillage\Repositories\EventsRepository')->find($id);
     });
     $router->get('events', [
         'as' => 'frontend.tradevillage.events.index',
         'uses' => 'FrontendEventController@index'
+    ]);
+    $router->get('events/{event}', [
+        'as' => 'frontend.tradevillage.events.show',
+        'uses' => 'FrontendEventController@show'
     ]);
     $router->bind('product', function ($id) {
         return app('Modules\TradeVillage\Repositories\ProductsRepository')->find($id);
--- Modules/TradeVillage/Resources/views/frontend/villages/products/partials/rate.blade.php
+++ Modules/TradeVillage/Resources/views/frontend/villages/products/partials/rate.blade.php
@@ -2,8 +2,8 @@
 	<p class="blue-text"><b>{{ mb_strtoupper(trans('tradevillage::products.rate'), 'UTF-8') }} ({{ count($product->rates) }} {{ trans('tradevillage::products.rate') }})</b></p>
 	
 	@if(Auth::user())
-		<input type="number" name="your_awesome_parameter" id="rating-readonly" class="rating rate-element orange-text"  value="{{ round($rate_average) }}"/>
+		<input type="number" name="your_awesome_parameter" id="rating-readonly" class="rating rate-element orange-text"  value="{{ round($product->rate) }}"/>
 	@else
-		<input type="number" name="your_awesome_parameter" id="rating-readonly" class="rating rate-element orange-text"  value="{{ round($rate_average) }}" data-readonly/>
+		<input type="number" name="your_awesome_parameter" id="rating-readonly" class="rating rate-element orange-text"  value="{{ round($product->rate) }}" data-readonly/>
 	@endif
 </div>
