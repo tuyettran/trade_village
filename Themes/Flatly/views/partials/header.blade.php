e46fa2d25dbc5e3d0c948233996393087a600457
tuyettran <tuyet.tran.k59@gmail.com>
2018-04-03 10:32:56 +0700

    fix layout navigation

--- Themes/Flatly/views/partials/header.blade.php
+++ Themes/Flatly/views/partials/header.blade.php
@@ -1,8 +1,9 @@
 <div class="super-header">
 	<img src="{{ URL::asset('/images/ntbic-header.png') }}" class="img-responsive">
-	@if($currentUser)
-		<div class="user sign top-right">
-				<ul class="nav navbar-nav navbar-right">
+    <div class="top-right">
+        <div class="user sign">
+            @if($currentUser)
+            <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown pull-right">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-flag"></i>
@@ -13,16 +14,57 @@
                     </a>
                     <ul class="dropdown-menu language-menu">
                         <li><a href="">{{ trans('tradevillage::main.title.my_profile') }}</a></li>
-	                	<li><a href="{{ route('frontend.tradevillage.products.user_products', $currentUser->id) }}">{{ trans('tradevillage::main.title.my_products') }}</a></li>
+                        <li><a href="{{ route('frontend.tradevillage.products.user_products', $currentUser->id) }}">{{ trans('tradevillage::main.title.my_products') }}</a></li>
                         <li><a href="{{ route('logout') }}">{{ trans('tradevillage::main.title.logout') }}</a></li>
                     </ul>
                 </li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown pull-right">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-flag"></i>
+                        <span><b>
+                            <img src="{{ asset('images/vi.png') }}" style="width: 20px; height: 20px">
+                            {{ LaravelLocalization::getCurrentLocaleName()  }}
+                            <i class="caret"></i></b>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu language-menu">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <li class="{{ App::getLocale() == $localeCode ? 'active' : '' }}">
+                                <a rel="alternate" lang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                    {!! $properties['native'] !!}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+            @else
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-flag"></i>
+                        <span><b>
+                            <img src="{{ asset('images/vi.png') }}" style="width: 20px; height: 20px">
+                            {{ LaravelLocalization::getCurrentLocaleName()  }}
+                            <i class="caret"></i></b>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu language-menu">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <li class="{{ App::getLocale() == $localeCode ? 'active' : '' }}">
+                                <a rel="alternate" lang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                    {!! $properties['native'] !!}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+            <a href="{{ route('login') }}" class="btn btn-warning black-text">{{ trans('tradevillage::main.title.login') }}</a>
+            <a href="{{ route('register') }}" class="btn btn-warning black-text">{{ trans('tradevillage::main.title.register_new') }}</a>
+            @endif
         </div>
-	@else
-		<div class="sign top-right">
-			<a href="{{ route('login') }}" class="btn btn-warning black-text">{{ trans('tradevillage::main.title.login') }}</a>
-			<a href="{{ route('register') }}" class="btn btn-warning black-text">{{ trans('tradevillage::main.title.register_new') }}</a>
-		</div>
-	@endif
+    </div>
 </div>
