1ab9be51842258d4a39705cdaabb9469bbc031e8
cuongvu <cuongvu@MacBook-Pro.net.fpt>
2018-04-12 21:14:25 +0700

    api-1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,12 +34,12 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class
         ],
 
         'api' => [
             'throttle:60,1',
-            'bindings',
+            'bindings'
         ],
     ];
 
